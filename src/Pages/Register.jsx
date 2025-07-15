6038871e007b7bfa94b384b810dd6c3db44cc23c
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-07-15 11:38:04 +0600

    handle multiple user and show error based on state called erroMessage

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,22 +1,25 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+import React, { useState } from 'react';
 import { auth } from '../firebase.init';
 
 const Register = () => {
-    const handleRegister = event => {
-        event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.email.value;
-        
+  const [errorMessage, setErrorMessage] = useState('');
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.email.value;
 
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            console.log('a user created', result.user)
-        })
-        .catch(error => {
-            console.log('Error', error)
-        })
-    }
+    setErrorMessage('')
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log('a user created', result.user);
+      })
+      .catch((error) => {
+        console.log('Error', error);
+        setErrorMessage(error.message);
+      });
+  };
   return (
     <div className="my-8">
       <h2 className="text-2xl font-bold text-center">Register</h2>
@@ -25,9 +28,19 @@ const Register = () => {
         <div className="card-body">
           <form onSubmit={handleRegister} className="fieldset">
             <label className="label">Email</label>
-            <input type="email" name="email" className="input" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="Email"
+            />
             <label className="label">Password</label>
-            <input type="password" name="password" className="input" placeholder="Password" />
+            <input
+              type="password"
+              name="password"
+              className="input"
+              placeholder="Password"
+            />
             <div>
               <a className="link link-hover">Forgot password?</a>
             </div>
@@ -35,6 +48,8 @@ const Register = () => {
           </form>
         </div>
       </div>
+
+      {errorMessage && <p className="text-red-500 text-center my-6">{errorMessage}</p>}
     </div>
   );
 };
